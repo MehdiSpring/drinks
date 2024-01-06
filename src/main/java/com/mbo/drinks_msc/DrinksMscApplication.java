a4eaeb0bbe7cf1333ce3264ae4aac419d0e60064
MehdiSpring <mehdi.boumzi@gmail.com>
2024-01-06 11:20:38 +0100

    Initialisation of the drinks msc project

---
+++ src/main/java/com/mbo/drinks_msc/DrinksMscApplication.java
@@ -0,0 +1,13 @@
+package com.mbo.drinks_msc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinksMscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinksMscApplication.class, args);
+	}
+
+}
